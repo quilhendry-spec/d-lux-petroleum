28833d8615ecba9ae726ed63180e566daa873bbf
D-Lux Petroleum <quilhendry@gmail.com>
2026-05-27 18:13:40 +0200

    Fix homepage route

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-5xl font-bold">
+        D-Lux Petroleum
+      </h1>
+    </main>
+  );
+}
